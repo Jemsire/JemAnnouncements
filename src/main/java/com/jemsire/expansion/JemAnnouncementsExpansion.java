f5736e502730fbe2987e86eee808eae18e9a675c
TinyTank800 <TinyTank800@gmail.com>
2026-02-11 18:38:04 -0800

    Updated JemPlaceholders Expansion

--- src/main/java/com/jemsire/expansion/JemAnnouncementsExpansion.java
+++ src/main/java/com/jemsire/expansion/JemAnnouncementsExpansion.java
@@ -1,65 +1,45 @@
 package com.jemsire.expansion;
 
-import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.jemsire.jemplaceholders.api.PlaceholderExpansion;
 import com.jemsire.plugin.AnnouncementPlugin;
 
 /**
- * JemLives placeholders.
+ * JemAnnouncement placeholders.
  */
 public class JemAnnouncementsExpansion extends PlaceholderExpansion {
 
-    @Override
-    public String getIdentifier() {
-        return "jemannouncements";
-    }
+    @Override public String getIdentifier() { return "jemannouncements"; }
 
-    @Override
-    public String getAuthor() {
-        return "Jemsire";
-    }
-
-    @Override
-    public String getVersion() {
-        return "1.0.0";
-    }
-
-    @Override
-    public String getName() {
+    @Override public String getName() {
         return "JemAnnouncements";
     }
 
-    @Override
-    public String getPluginNamespace() {
+    @Override public String getPluginNamespace() {
         return "Jemsire:JemAnnouncements";
     }
 
-    @Override
-    public String getDescription() {
+    @Override public String getDescription() {
         return "Provides placeholders for JemAnnouncements information.";
     }
 
-    @Override
-    public String onPlaceholderRequest(PlayerRef player, String params) {
-        if (player == null) {
-            return "";
-        }
+    @Override public String getAuthor() { return "Jemsire"; }
 
-        if (params.equalsIgnoreCase("size")) {
+    @Override public String getVersion() { return "1.0.0"; }
 
-            return "" + AnnouncementPlugin.get().getMessageConfigs().size();
-        }
+    @Override public String getWebsite() { return "https://www.curseforge.com/members/jemsire/projects"; }
 
-        if (params.equalsIgnoreCase("interval")) {
+    public JemAnnouncementsExpansion() {
 
-            return AnnouncementPlugin.get().getAnnouncementConfig().get().getIntervalSeconds() + " Seconds.";
-        }
+        exact("size", (player, params) ->
+                AnnouncementPlugin.get().getMessageConfigs().size() + ""
+        );
 
-        if (params.equalsIgnoreCase("randomize")) {
+        exact("interval", (player, params) ->
+                AnnouncementPlugin.get().getAnnouncementConfig().get().getIntervalSeconds() + " Seconds."
+        );
 
-            return "" + AnnouncementPlugin.get().getAnnouncementConfig().get().isEnableRandomization();
-        }
-
-        return null;
+        exact("randomize", (player, params) ->
+                AnnouncementPlugin.get().getAnnouncementConfig().get().isEnableRandomization() + ""
+        );
     }
 }
